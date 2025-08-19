64bc337351325e0c569fb0f822e5cc8006efe6dc
Nathalia Modesto <modestonathalia52@gmail.com>
2025-08-19 16:04:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,49 +3,46 @@ const app = express();
 const PORT = 8081;
 
 
-app.get("/imc", (req, res) => {
+app.get('/saudacao/:nome', (req, res) => {
 
+    try {
+
+    const nome = req.params.nome;
+    const hora = parseInt(req.query.hora);
     
-    const peso = parseFloat(req.query.peso);
-    const altura = parseFloat(req.query.altura);
-
-    if (isNaN(peso) || isNaN(altura) || altura === 0) {
-        return res.status(400).send('Erro! Forneça valores válidos para peso e altura.');
+    if (isNaN(hora) || hora < 0 || hora > 23) {
+        return res.status(400).send('Erro: Forneça uma hora válida (0-23).');
     }
-
-    const imc = peso / (altura * altura);
-    let classificacao;
-
-
-    if (imc < 18.5) {
-
-        classificacao = 'Baixo peso';
-
+    
+    let saudacao;
+    if (hora >= 5 && hora <= 11) {
+        saudacao = 'Bom dia';
     } 
 
-    else if (imc >= 18.5 && imc <= 24.9) {
-        classificacao = 'Peso normal';
-
-
+    else if (hora >= 12 && hora <= 17) {
+        saudacao = 'Boa tarde';
+    }
+    
+    else if (hora >= 18 && hora <= 23) {
+        saudacao = 'Boa noite';
     } 
     
-    else if (imc >= 25 && imc <= 29.9) {
-        classificacao = 'Sobrepeso';
-    } 
+    else { 
+        saudacao = 'Boa madrugada';
+    }
     
-    else if (imc >= 30 && imc <= 34.9) {
-             classificacao = 'Obesidade';
-        }
-       
+
+    res.send(`${saudacao}, ${nome}!`);
     
     
-   
-    res.send(`IMC: ${imc.toFixed(2)} - ${classificacao}`);
+} catch (error) {
+    
+    console.error('Erro ao processar a saudação:', error);
+    return res.status(500).send('Erro interno do servidor.');
+ }
+
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
